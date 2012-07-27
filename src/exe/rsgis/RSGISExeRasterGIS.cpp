e87cdcf8e7bb30381d7b5ab230672232ef2e6055
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2012-07-27 22:26:40 +1200

    Created XML interface for two new commands within the new rastergis interface.
    1) Calculate the mean eastings and northings for a clump
    2) Calculate the euclidean distance from a feature to all the others - outputting as a new column.

--- src/exe/rsgis/RSGISExeRasterGIS.cpp
+++ src/exe/rsgis/RSGISExeRasterGIS.cpp
@@ -38,7 +38,6 @@ namespace rsgisexe{
     
     void RSGISExeRasterGIS::retrieveParameters(xercesc::DOMElement *argElement) throw(rsgis::RSGISXMLArgumentsException)
     {	
-        rsgis::math::RSGISMathsUtils mathUtils;
         rsgis::utils::RSGISTextUtils textUtils;
         
         XMLCh *algorName = xercesc::XMLString::transcode(this->algorithm.c_str());
@@ -46,6 +45,8 @@ namespace rsgisexe{
         XMLCh *optionXMLStr = xercesc::XMLString::transcode("option");
         
         XMLCh *optionCopyGDALATT = xercesc::XMLString::transcode("copyGDALATT");
+        XMLCh *optionSpatialLocation = xercesc::XMLString::transcode("spatiallocation");
+        XMLCh *optionEucDistFromFeat = xercesc::XMLString::transcode("eucdistfromfeat");
         
         
         const XMLCh *algorNameEle = argElement->getAttribute(algorXMLStr);
@@ -53,8 +54,6 @@ namespace rsgisexe{
         {
             throw rsgis::RSGISXMLArgumentsException("The algorithm name is incorrect.");
         }
-        
-
         
         const XMLCh *optionXML = argElement->getAttribute(optionXMLStr);
         if(xercesc::XMLString::equals(optionCopyGDALATT, optionXML))
@@ -88,6 +87,125 @@ namespace rsgisexe{
             }
             xercesc::XMLString::release(&imageXMLStr);
         }
+        else if(xercesc::XMLString::equals(optionSpatialLocation, optionXML))
+        {		
+            this->option = RSGISExeRasterGIS::spatiallocation;
+            
+            XMLCh *imageXMLStr = xercesc::XMLString::transcode("image");
+            if(argElement->hasAttribute(imageXMLStr))
+            {
+                char *charValue = xercesc::XMLString::transcode(argElement->getAttribute(imageXMLStr));
+                this->inputImage = std::string(charValue);
+                xercesc::XMLString::release(&charValue);
+            }
+            else
+            {
+                throw rsgis::RSGISXMLArgumentsException("No \'image\' attribute was provided.");
+            }
+            xercesc::XMLString::release(&imageXMLStr);
+            
+            XMLCh *eastingsXMLStr = xercesc::XMLString::transcode("eastings");
+            if(argElement->hasAttribute(eastingsXMLStr))
+            {
+                char *charValue = xercesc::XMLString::transcode(argElement->getAttribute(eastingsXMLStr));
+                this->eastingsField = std::string(charValue);
+                xercesc::XMLString::release(&charValue);
+            }
+            else
+            {
+                throw rsgis::RSGISXMLArgumentsException("No \'eastings\' attribute was provided.");
+            }
+            xercesc::XMLString::release(&eastingsXMLStr);
+            
+            XMLCh *northingsXMLStr = xercesc::XMLString::transcode("northings");
+            if(argElement->hasAttribute(northingsXMLStr))
+            {
+                char *charValue = xercesc::XMLString::transcode(argElement->getAttribute(northingsXMLStr));
+                this->northingsField = std::string(charValue);
+                xercesc::XMLString::release(&charValue);
+            }
+            else
+            {
+                throw rsgis::RSGISXMLArgumentsException("No \'northings\' attribute was provided.");
+            }
+            xercesc::XMLString::release(&northingsXMLStr);
+        }
+        else if(xercesc::XMLString::equals(optionEucDistFromFeat, optionXML))
+        {		
+            this->option = RSGISExeRasterGIS::eucdistfromfeat;
+            
+            XMLCh *imageXMLStr = xercesc::XMLString::transcode("image");
+            if(argElement->hasAttribute(imageXMLStr))
+            {
+                char *charValue = xercesc::XMLString::transcode(argElement->getAttribute(imageXMLStr));
+                this->inputImage = std::string(charValue);
+                xercesc::XMLString::release(&charValue);
+            }
+            else
+            {
+                throw rsgis::RSGISXMLArgumentsException("No \'image\' attribute was provided.");
+            }
+            xercesc::XMLString::release(&imageXMLStr);
+            
+            XMLCh *outFieldXMLStr = xercesc::XMLString::transcode("outfield");
+            if(argElement->hasAttribute(outFieldXMLStr))
+            {
+                char *charValue = xercesc::XMLString::transcode(argElement->getAttribute(outFieldXMLStr));
+                this->outputField = std::string(charValue);
+                xercesc::XMLString::release(&charValue);
+            }
+            else
+            {
+                throw rsgis::RSGISXMLArgumentsException("No \'outfield\' attribute was provided.");
+            }
+            xercesc::XMLString::release(&outFieldXMLStr);
+            
+            XMLCh *featureXMLStr = xercesc::XMLString::transcode("feature");
+            if(argElement->hasAttribute(featureXMLStr))
+            {
+                char *charValue = xercesc::XMLString::transcode(argElement->getAttribute(featureXMLStr));
+                this->fid = textUtils.strtosizet(std::string(charValue));
+                xercesc::XMLString::release(&charValue);
+            }
+            else
+            {
+                throw rsgis::RSGISXMLArgumentsException("No \'feature\' attribute was provided.");
+            }
+            xercesc::XMLString::release(&featureXMLStr);
+            
+            XMLCh *rsgisFieldXMLStr = xercesc::XMLString::transcode("rsgis:field");
+            xercesc::DOMNodeList *fieldNodesList = argElement->getElementsByTagName(rsgisFieldXMLStr);
+            unsigned int numFieldTags = fieldNodesList->getLength();
+            
+            std::cout << "Found " << numFieldTags << " field tags" << std::endl;
+            
+            if(numFieldTags == 0)
+            {
+                throw rsgis::RSGISXMLArgumentsException("No field tags have been provided, at least 1 is required.");
+            }
+            
+            fields.reserve(numFieldTags);
+            
+            xercesc::DOMElement *attElement = NULL;
+            std::string fieldName = "";
+            for(int i = 0; i < numFieldTags; i++)
+            {
+                attElement = static_cast<xercesc::DOMElement*>(fieldNodesList->item(i));
+                
+                XMLCh *nameXMLStr = xercesc::XMLString::transcode("name");
+                if(attElement->hasAttribute(nameXMLStr))
+                {
+                    char *charValue = xercesc::XMLString::transcode(attElement->getAttribute(nameXMLStr));
+                    fields.push_back(std::string(charValue));
+                    xercesc::XMLString::release(&charValue);
+                }
+                else
+                {
+                    throw rsgis::RSGISXMLArgumentsException("No \'name\' attribute was provided.");
+                }
+                xercesc::XMLString::release(&nameXMLStr);
+            }  
+        }
         else
         {
             std::string message = std::string("The option (") + std::string(xercesc::XMLString::transcode(optionXML)) + std::string(") is not known: RSGISExeRasterGIS.");
@@ -100,6 +218,7 @@ namespace rsgisexe{
         xercesc::XMLString::release(&algorXMLStr);
         xercesc::XMLString::release(&optionXMLStr);
         xercesc::XMLString::release(&optionCopyGDALATT);
+        xercesc::XMLString::release(&optionSpatialLocation);
         
     }
     
@@ -149,6 +268,30 @@ namespace rsgisexe{
                     throw e;
                 }	
             }
+            else if(this->option == RSGISExeRasterGIS::spatiallocation)
+            {
+                std::cout << "Populate the attribute table with mean eastings and northings\n";
+                std::cout << "Input Image: " << this->inputImage << std::endl;
+                std::cout << "Eastings Field: " << this->eastingsField << std::endl;
+                std::cout << "Northings Field: " << this->northingsField << std::endl;
+                
+                
+            }
+            else if(this->option == RSGISExeRasterGIS::eucdistfromfeat)
+            {
+                std::cout << "Calculate the euclidean distance from a feature to all other features.\n";
+                std::cout << "Input Image: " << this->inputImage << std::endl;
+                std::cout << "Feature: " << this->fid << std::endl;
+                std::cout << "Output Field: " << this->outputField << std::endl;
+                std::cout << "Distance calculated using:\n";
+                for(std::vector<std::string>::iterator iterFields = fields.begin(); iterFields != fields.end(); ++iterFields)
+                {
+                    std::cout << "\tField: " << (*iterFields) << std::endl;
+                }
+                
+                
+                
+            }
             else
             {
                 throw rsgis::RSGISException("The option is not recognised: RSGISExeRasterGIS");
@@ -165,6 +308,29 @@ namespace rsgisexe{
                 std::cout << "Export an attribute table to a GDAL Dataset with a raster attribute table\n";
                 std::cout << "Input Table: " << this->inputImage << std::endl;
                 std::cout << "Output Image: " << this->clumpsImage << std::endl;
+            }
+            else if(this->option == RSGISExeRasterGIS::spatiallocation)
+            {
+                std::cout << "Populate the attribute table with mean eastings and northings\n";
+                std::cout << "Input Image: " << this->inputImage << std::endl;
+                std::cout << "Eastings Field: " << this->eastingsField << std::endl;
+                std::cout << "Northings Field: " << this->northingsField << std::endl;
+            }
+            else if(this->option == RSGISExeRasterGIS::eucdistfromfeat)
+            {
+                std::cout << "Calculate the euclidean distance from a feature to all other features.\n";
+                std::cout << "Input Image: " << this->inputImage << std::endl;
+                std::cout << "Feature: " << this->fid << std::endl;
+                std::cout << "Output Field: " << this->outputField << std::endl;
+                std::cout << "Distance calculated using:\n";
+                for(std::vector<std::string>::iterator iterFields = fields.begin(); iterFields != fields.end(); ++iterFields)
+                {
+                    std::cout << "\tField: " << (*iterFields) << std::endl;
+                }
+            }
+            else
+            {
+                std::cout << "The option was not recognised: RSGISExeRasterGIS\n";
             }
         }
         else
