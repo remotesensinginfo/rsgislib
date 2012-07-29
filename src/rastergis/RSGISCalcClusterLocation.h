22c8c06054466548dddcaab1cda12ce493136df4
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2012-07-29 21:40:59 +1200

    Update to include new implementations of commands for calculating the mean spatial location for each clump and a command to calculate the euclidean distance for a feature to all other features using a selection of columns.

---
+++ src/rastergis/RSGISCalcClusterLocation.h
@@ -0,0 +1,76 @@
+/*
+ *  RSGISCalcClusterLocation.h
+ *  RSGIS_LIB
+ *
+ *  Created by Pete Bunting on 28/07/2012.
+ *  Copyright 2012 RSGISLib.
+ * 
+ *  RSGISLib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RSGISLib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RSGISLib.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef RSGISCalcClusterLocation_H
+#define RSGISCalcClusterLocation_H
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#include "gdal_priv.h"
+#include "gdal_rat.h"
+
+#include "common/RSGISAttributeTableException.h"
+
+#include "img/RSGISImageCalcException.h"
+#include "img/RSGISCalcImageValue.h"
+#include "img/RSGISCalcImage.h"
+
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/lexical_cast.hpp>
+
+namespace rsgis{namespace rastergis{
+	
+    class RSGISCalcClusterLocation
+    {
+    public:
+        RSGISCalcClusterLocation();
+        void populateAttWithClumpLocation(GDALDataset *dataset, std::string eastColumn, std::string northColumn)throw(rsgis::RSGISAttributeTableException);
+        ~RSGISCalcClusterLocation();
+    };
+    
+    
+    
+	class RSGISCalcClusterLocationCalcValue : public rsgis::img::RSGISCalcImageValue
+	{
+	public: 
+		RSGISCalcClusterLocationCalcValue(GDALRasterAttributeTable *attTable, int countColIdx, int eastColIdx, int northColIdx);
+		void calcImageValue(float *bandValues, int numBands, float *output) throw(rsgis::img::RSGISImageCalcException){throw rsgis::img::RSGISImageCalcException("No implemented");};
+		void calcImageValue(float *bandValues, int numBands) throw(rsgis::img::RSGISImageCalcException){throw rsgis::img::RSGISImageCalcException("No implemented");};
+		void calcImageValue(float *bandValues, int numBands, geos::geom::Envelope extent) throw(rsgis::img::RSGISImageCalcException);
+		void calcImageValue(float *bandValues, int numBands, float *output, geos::geom::Envelope extent) throw(rsgis::img::RSGISImageCalcException){throw rsgis::img::RSGISImageCalcException("No implemented");};
+		void calcImageValue(float ***dataBlock, int numBands, int winSize, float *output) throw(rsgis::img::RSGISImageCalcException){throw rsgis::img::RSGISImageCalcException("No implemented");};
+        void calcImageValue(float ***dataBlock, int numBands, int winSize, float *output, geos::geom::Envelope extent) throw(rsgis::img::RSGISImageCalcException){throw rsgis::img::RSGISImageCalcException("No implemented");};
+		bool calcImageValueCondition(float ***dataBlock, int numBands, int winSize, float *output) throw(rsgis::img::RSGISImageCalcException){throw rsgis::img::RSGISImageCalcException("No implemented");};
+		~RSGISCalcClusterLocationCalcValue();
+    private:
+        GDALRasterAttributeTable *attTable;
+        int countColIdx;
+        int eastColIdx;
+        int northColIdx;
+	};
+	
+}}
+
+#endif
+
