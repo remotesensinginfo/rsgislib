ce94ce3a13257743a5309910f2c803ff25aa7b84
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2013-08-19 23:28:37 +0100

    New command to remove objects below an area threshold by setting their pixel value to 0 (i.e., background).

--- python/rsgislib/__init__.py
+++ python/rsgislib/__init__.py
@@ -1,5 +1,60 @@
 """
-This namespace contains rsgislib Python bindings
+This namespace contains rsgislib Python bindings 
+
+Please be aware that the following variables have been 
+defined to match enums within RSGISLib.
+
+Data Types for images:
+
+    * TYPE_UNDEFINED = 0
+    * TYPE_8INT = 1
+    * TYPE_16INT = 2
+    * TYPE_32INT = 3
+    * TYPE_64INT = 4
+    * TYPE_8UINT = 5
+    * TYPE_16UINT = 6
+    * TYPE_32UINT = 7
+    * TYPE_64UINT = 8
+    * TYPE_32FLOAT = 9
+    * TYPE_64FLOAT = 10
+
+TODO: What are these for???
+
+    * METHOD_SAMPLES = 0
+    * METHOD_AREA = 1
+    * METHOD_EQUAL = 2
+    * METHOD_USERDEFINED = 3
+    * METHOD_WEIGHTED = 4
+
+Shape indexes used with RasterGIS:
+
+    * SHAPE_SHAPENA = 0
+    * SHAPE_SHAPEAREA = 1
+    * SHAPE_ASYMMETRY = 2
+    * SHAPE_BORDERINDEX = 3
+    * SHAPE_BORDERLENGTH = 4
+    * SHAPE_COMPACTNESS = 5
+    * SHAPE_DENSITY = 6
+    * SHAPE_ELLIPTICFIT = 7
+    * SHAPE_LENGTH = 8
+    * SHAPE_LENGTHWIDTH = 9
+    * SHAPE_WIDTH = 10
+    * SHAPE_MAINDIRECTION = 11
+    * SHAPE_RADIUSLARGESTENCLOSEDELLIPSE = 12
+    * SHAPE_RADIUSSMALLESTENCLOSEDELLIPSE = 13
+    * SHAPE_RECTANGULARFIT = 14
+    * SHAPE_ROUNDNESS = 15
+    * SHAPE_SHAPEINDEX = 16
+
+Methods of initilising KMEANS:
+
+    * INITCLUSTER_RANDOM = 0
+    * INITCLUSTER_DIAGONAL_FULL = 1
+    * INITCLUSTER_DIAGONAL_STDDEV = 2
+    * INITCLUSTER_DIAGONAL_FULL_ATTACH = 3
+    * INITCLUSTER_DIAGONAL_STDDEV_ATTACH = 4
+    * INITCLUSTER_KPP = 5
+
 """
 import os.path
 import os
@@ -49,7 +104,10 @@ INITCLUSTER_DIAGONAL_STDDEV_ATTACH = 4
 INITCLUSTER_KPP = 5
 
 class RSGISPyException(Exception):
-
+    """
+    A class representing the RSGIS exception.
+    """
+    
     def __init__(self, value):
         """
         Init for the RSGISPyException class
@@ -68,6 +126,10 @@ class RSGISPyUtils (object):
     """
     
     def getFileExtension(self, format):
+        """
+        A function to get the extension for a given file format 
+        (NOTE, currently only KEA, GTIFF, HFA and ENVI are supported).
+        """
         ext = ".NA"
         if format.lower() == "kea":
             ext = ".kea"
@@ -82,6 +144,10 @@ class RSGISPyUtils (object):
         return ext
     
     def deleteFileWithBasename(self, filePath):
+        """
+        Function to delete all the files which have a path
+        and base name defined in the filePath attribute.
+        """
         fileDIR = os.path.split(filePath)[0]
         fileName = os.path.split(filePath)[1]
         
@@ -93,6 +159,11 @@ class RSGISPyUtils (object):
                     os.remove(os.path.join(fileDIR, file))
                 
     def deleteDIR(self, dirPath):
+        """
+        A function which will delete a directory, if files and other directories
+        are within the path specified they will be recursively deleted as well.
+        So becareful you don't delete things within meaning it.
+        """
         for root, dirs, files in os.walk(dirPath, topdown=False):
             for name in files:
                 os.remove(os.path.join(root, name))
