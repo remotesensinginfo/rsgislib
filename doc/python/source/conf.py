a63d3c1084d5ee78b4806d168f4ddb06f1fd4e70
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2013-10-14 17:32:29 +0100

    Minor changes to documentation

--- doc/python/source/conf.py
+++ doc/python/source/conf.py
@@ -192,10 +192,10 @@ extlinks = {'issue': ('https://bitbucket.org/petebunting/rsgislib/issue/%s/',
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
