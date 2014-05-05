cc750d265cb18a9bd30ae6a36a1f3535a1f0dfad
Daniel Clewley <daniel.clewley@gmail.com>
2014-05-04 22:38:11 -0700

    Added link to ARCSI

--- python/rsgislib/imagecalibration/__init__.py
+++ python/rsgislib/imagecalibration/__init__.py
@@ -1,5 +1,10 @@
 """
-The image callibration module contains functions for calibrating optical data from DN to radience and top of atmosphere reflectance and, using coefficients from 6S, surface reflectance.
+The image calibration module contains functions for calibrating optical data from DN to radience and top of atmosphere reflectance and, using coefficients from 6S, surface reflectance.
+
+For obtaining the correct parameters for each function it is recomented that rather than running directly they are called through the Atmospheric and Radiometric Correction of Satellite Imagery (ARCSI) software. 
+
+More details on ARCSI are avaialble from http://rsgislib.org/arcsi
+
 """
 
 # import the C++ extension into this level
