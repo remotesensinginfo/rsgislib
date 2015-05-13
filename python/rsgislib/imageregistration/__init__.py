ad2c82f69a985a2d0765f722383aae3ec745397c
Pete Bunting <petebunting@mac.com>
2015-05-13 08:07:32 +0100

    and another one.

--- python/rsgislib/imageregistration/__init__.py
+++ python/rsgislib/imageregistration/__init__.py
@@ -4,7 +4,6 @@ The image registration module contains algorithms for generating tie points matc
 There are two algorithms are available for registration: basic, and singlelayer. The single layer algorithm is a simplified version of the algorithm proposed in:
 
 Bunting, P.J., Labrosse, F. & Lucas, R.M., 2010. A multi-resolution area-based technique for automatic multi-modal image registration. Image and Vision Computing, 28(8), pp.1203–1219.
-
 """
 
 # import the C++ extension into this level
