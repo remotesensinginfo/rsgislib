3e8f45476aa48bfbab295470f9af04c1dbb51733
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2017-01-22 05:48:05 +0900

    Added to registration documentation.

--- python/rsgislib/imageregistration/__init__.py
+++ python/rsgislib/imageregistration/__init__.py
@@ -4,6 +4,24 @@ The image registration module contains algorithms for generating tie points matc
 There are two algorithms are available for registration: basic, and singlelayer. The single layer algorithm is a simplified version of the algorithm proposed in:
 
 Bunting, P.J., Labrosse, F. & Lucas, R.M., 2010. A multi-resolution area-based technique for automatic multi-modal image registration. Image and Vision Computing, 28(8), pp.1203-1219.
+
+
+Image distance metrics:
+
+    * METRIC_EUCLIDEAN = 1
+    * METRIC_SQDIFF = 2
+    * METRIC_MANHATTEN = 3
+    * METRIC_CORELATION = 4
+
+
+GCP Output Types:
+
+    * TYPE_ENVI_IMG2IMG = 1
+    * TYPE_ENVI_IMG2MAP = 2
+    * TYPE_RSGIS_IMG2MAP = 3
+    * TYPE_RSGIS_MAPOFFS = 4
+
+
 """
 
 # import the C++ extension into this level
