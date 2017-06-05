f547f473f939f91d84b4025f7f5fdc7a9de33703
Pete Bunting <petebunting@mac.com>
2017-06-05 14:57:54 +0100

    Tidying up some of the documentation.

--- python/rsgislib/tools/filemanipulation.py
+++ python/rsgislib/tools/filemanipulation.py
@@ -12,16 +12,17 @@ import shutil
 
 def sortImgsUTM2DIRs(inputImgsDIR, fileSearchStr, outBaseDIR):
     """
-    A function which will sort a series of input image files which
-    a projected using the UTM system into individual directories per
-    UTM zone. Please note that the input files are moved on your system!!
-    
-    Where:
-    
-    * inputImgsDIR - directory where the input files are to be found.
-    * fileSearchStr - the wildcard search string to find files within the input directory (e.g., *.kea).
-    * outBaseDIR - the output directory where the UTM folders will be created and the files copied.
-    """
+A function which will sort a series of input image files which
+a projected using the UTM system into individual directories per
+UTM zone. Please note that the input files are moved on your system!!
+
+Where:
+
+* inputImgsDIR - directory where the input files are to be found.
+* fileSearchStr - the wildcard search string to find files within the input directory (e.g., *.kea).
+* outBaseDIR - the output directory where the UTM folders will be created and the files copied.
+
+"""
     rsgisUtils = rsgislib.RSGISPyUtils()
     inFiles = glob.glob(os.path.join(inputImgsDIR, fileSearchStr))
     for imgFile in inFiles:
