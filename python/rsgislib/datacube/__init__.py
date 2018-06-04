19585b4d6322e8e9c7db8ac142a7c7e5eaefd81e
Pete Bunting <petebunting@mac.com>
2018-06-04 18:09:51 +0100

    Add a new module for interfacing with the open data cube. First function does a basic dataset retrieval from a datacube and saves to a gdal supported file format.

---
+++ python/rsgislib/datacube/__init__.py
@@ -0,0 +1,13 @@
+"""
+The datacube module provides a link to the open data cube within RSGISLib.
+"""
+
+# import the C++ extension into this level
+#from ._datacube import *
+
+
+
+
+    
+    
+    
