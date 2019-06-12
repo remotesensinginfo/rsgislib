765ab92279450c06de86d85738f83f358d589a6e
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2019-06-12 23:50:01 +0100

    Update with new function extractImgPxlValsInMsk to get pixel values returned as a number array. Also, a new module changedetect with a new function to find within class outliers using the pyod library.

---
+++ python/rsgislib/changedetect/__init__.py
@@ -0,0 +1,6 @@
+"""
+The changedetect module provides tools for change detection using remotely sensed imagery.
+"""
+
+# import the C++ extension into this level (when/if implemented)
+#from ._changedetect import *
