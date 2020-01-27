5e2668fccdfc13768b2b6256b13c6b759926b639
Pete Bunting <petebunting@mac.com>
2020-01-27 11:39:26 +0000

    Update with new module for species distribution modelling - written by Osian Roberts. This is a first version which is complete and running but in the future it needs to be refactored for coding style and to us RIOS for image processing rather than using GDAL directly.

---
+++ python/rsgislib/sdm/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+The Species Distrubution Modelling (SDM) module provides a set of tools and utilities for
+undertaking a species distribution modelling project.
+
+These functions have been developed by Osian Roberts.
+
+"""
+
+from .pysdm import *
+
