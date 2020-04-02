55e88c9c895da48cfa3337201e88f00678c906f1
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2020-04-02 17:07:31 +0100

    Update with functions for lightgbm and xgboost binary classification to separate the parameter optimisation steps and the final training with the optimised parameters saved as a JSON file.

--- python/rsgislib/classification/classxgboost.py
+++ python/rsgislib/classification/classxgboost.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 ############################################################################
-#  classimgutils.py
+#  classxgboost.py
 #
-#  Copyright 2016 RSGISLib.
+#  Copyright 2020 RSGISLib.
 #
 #  RSGISLib: 'The remote sensing and GIS Software Library'
 #
@@ -26,7 +26,7 @@
 #
 # Author: Pete Bunting
 # Email: petebunting@mac.com
-# Date: 17/12/2016
+# Date: 16/02/2020
 # Version: 1.0
 #
 # History:
@@ -60,9 +60,247 @@ from sklearn.metrics import accuracy_score
 
 import gc
 
+import json
 
-def train_xgboost_binary_classifer(out_mdl_file, cls1_train_file, cls1_valid_file, cls1_test_file, cls2_train_file,
-                                   cls2_valid_file, cls2_test_file, nthread=2, scale_pos_weight=None, mdl_cls_obj=None):
+def optimise_xgboost_binary_classifer(out_params_file, cls1_train_file, cls1_valid_file, cls2_train_file,
+                                      cls2_valid_file, nthread=2, scale_pos_weight=None, mdl_cls_obj=None):
+    """
+    A function which performs a bayesian optimisation of the hyper-parameters for a binary xgboost
+    classifier. Class 1 is the class which you are interested in and Class 2 is the 'other class'.
+
+    This function requires that xgboost and skopt modules to be installed.
+
+    :param out_params_file: The output model parameters which have been optimised.
+    :param cls1_train_file: Training samples HDF5 file for the primary class (i.e., the one being classified)
+    :param cls1_valid_file: Validation samples HDF5 file for the primary class (i.e., the one being classified)
+    :param cls1_test_file: Testing samples HDF5 file for the primary class (i.e., the one being classified)
+    :param cls2_train_file: Training samples HDF5 file for the 'other' class
+    :param cls2_valid_file: Validation samples HDF5 file for the 'other' class
+    :param cls2_test_file: Testing samples HDF5 file for the 'other' class
+    :param nthread: The number of threads to use for the training.
+    :param scale_pos_weight: Optional, default is None. If None then a value will automatically be calculated.
+                             Parameter used to balance imbalanced training data.
+    :param mdl_cls_obj: XGBoost object to allow continue training with a new dataset.
+
+    """
+    print("Reading Class 1 Training")
+    f = h5py.File(cls1_train_file, 'r')
+    num_cls1_train_rows = f['DATA/DATA'].shape[0]
+    print("num_cls1_train_rows = {}".format(num_cls1_train_rows))
+    train_cls1 = numpy.array(f['DATA/DATA'])
+    train_cls1_lbl = numpy.ones(num_cls1_train_rows, dtype=int)
+
+    print("Reading Class 1 Validation")
+    f = h5py.File(cls1_valid_file, 'r')
+    num_cls1_valid_rows = f['DATA/DATA'].shape[0]
+    print("num_cls1_valid_rows = {}".format(num_cls1_valid_rows))
+    valid_cls1 = numpy.array(f['DATA/DATA'])
+    valid_cls1_lbl = numpy.ones(num_cls1_valid_rows, dtype=int)
+
+    print("Reading Class 2 Training")
+    f = h5py.File(cls2_train_file, 'r')
+    num_cls2_train_rows = f['DATA/DATA'].shape[0]
+    print("num_cls2_train_rows = {}".format(num_cls2_train_rows))
+    train_cls2 = numpy.array(f['DATA/DATA'])
+    train_cls2_lbl = numpy.zeros(num_cls2_train_rows, dtype=int)
+
+    print("Reading Class 2 Validation")
+    f = h5py.File(cls2_valid_file, 'r')
+    num_cls2_valid_rows = f['DATA/DATA'].shape[0]
+    print("num_cls2_valid_rows = {}".format(num_cls2_valid_rows))
+    valid_cls2 = numpy.array(f['DATA/DATA'])
+    valid_cls2_lbl = numpy.zeros(num_cls2_valid_rows, dtype=int)
+
+    print("Finished Reading Data")
+
+    vaild_np = numpy.concatenate((valid_cls2, valid_cls1))
+    vaild_lbl_np = numpy.concatenate((valid_cls2_lbl, valid_cls1_lbl))
+    d_valid = xgb.DMatrix(vaild_np, label=vaild_lbl_np)
+
+    d_train = xgb.DMatrix(numpy.concatenate((train_cls2, train_cls1)),
+                          label=numpy.concatenate((train_cls2_lbl, train_cls1_lbl)))
+
+    space = [Real(0.01, 0.9, name='eta'),
+             Integer(0, 100, name='gamma'),
+             Integer(2, 20, name='max_depth'),
+             Integer(1, 10, name='min_child_weight'),
+             Integer(0, 10, name='max_delta_step'),
+             Real(0.5, 1, name='subsample'),
+             Integer(2, 100, name='num_boost_round')
+             ]
+
+    if scale_pos_weight is None:
+        scale_pos_weight = num_cls2_train_rows / num_cls1_train_rows
+        if scale_pos_weight < 1:
+            scale_pos_weight = 1
+    print("scale_pos_weight = {}".format(scale_pos_weight))
+
+    def _objective(values):
+        params = {'eta'             : values[0],
+                  'gamma'           : values[1],
+                  'max_depth'       : values[2],
+                  'min_child_weight': values[3],
+                  'max_delta_step'  : values[4],
+                  'subsample'       : values[5],
+                  'nthread'         : nthread,
+                  'eval_metric'     : 'auc',
+                  'objective'       : 'binary:logistic'}
+
+        print('\nNext set of params.....', params)
+
+        num_boost_round = values[6]
+        print("num_boost_round = {}.".format(num_boost_round))
+
+        watchlist = [(d_train, 'train'), (d_valid, 'validation')]
+        evals_results = {}
+        model_xgb = xgb.train(params, d_train, num_boost_round, evals=watchlist, evals_result=evals_results,
+                              verbose_eval=False, xgb_model=mdl_cls_obj)
+
+        auc = -roc_auc_score(vaild_lbl_np, model_xgb.predict(d_valid))
+        print('\nAUROC.....', -auc, ".....iter.....")
+        gc.collect()
+        return auc
+
+    res_gp = gp_minimize(_objective, space, n_calls=20, random_state=0, n_random_starts=10)
+
+    print("Best score={}".format(res_gp.fun))
+
+    best_params = res_gp.x
+
+    params = {'eta'             : float(best_params[0]),
+              'gamma'           : int(best_params[1]),
+              'max_depth'       : int(best_params[2]),
+              'min_child_weight': int(best_params[3]),
+              'max_delta_step'  : int(best_params[4]),
+              'subsample'       : float(best_params[5]),
+              'nthread'         : int(nthread),
+              'eval_metric'     : 'auc',
+              'objective'       : 'binary:logistic',
+              'num_boost_round' : int(best_params[6])}
+
+    with open(out_params_file, 'w') as fp:
+        json.dump(params, fp, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
+
+
+def train_xgboost_binary_classifer(out_mdl_file, cls_params_file, cls1_train_file, cls1_valid_file, cls1_test_file,
+                                   cls2_train_file, cls2_valid_file, cls2_test_file, nthread=2, mdl_cls_obj=None):
+    """
+    A function which performs a bayesian optimisation of the hyper-parameters for a binary xgboost
+    classifier. Class 1 is the class which you are interested in and Class 2 is the 'other class'.
+
+    This function requires that xgboost and skopt modules to be installed.
+
+    :param out_mdl_file: The output model which can be loaded to perform a classification.
+    :param cls_params_file: A JSON file with the model parameters
+    :param cls1_train_file: Training samples HDF5 file for the primary class (i.e., the one being classified)
+    :param cls1_valid_file: Validation samples HDF5 file for the primary class (i.e., the one being classified)
+    :param cls1_test_file: Testing samples HDF5 file for the primary class (i.e., the one being classified)
+    :param cls2_train_file: Training samples HDF5 file for the 'other' class
+    :param cls2_valid_file: Validation samples HDF5 file for the 'other' class
+    :param cls2_test_file: Testing samples HDF5 file for the 'other' class
+    :param nthread: The number of threads to use for the training.
+    :param scale_pos_weight: Optional, default is None. If None then a value will automatically be calculated.
+                             Parameter used to balance imbalanced training data.
+    :param mdl_cls_obj: XGBoost object to allow continue training with a new dataset.
+
+    """
+    print("Reading Class 1 Training")
+    f = h5py.File(cls1_train_file, 'r')
+    num_cls1_train_rows = f['DATA/DATA'].shape[0]
+    print("num_cls1_train_rows = {}".format(num_cls1_train_rows))
+    train_cls1 = numpy.array(f['DATA/DATA'])
+    train_cls1_lbl = numpy.ones(num_cls1_train_rows, dtype=int)
+
+    print("Reading Class 1 Validation")
+    f = h5py.File(cls1_valid_file, 'r')
+    num_cls1_valid_rows = f['DATA/DATA'].shape[0]
+    print("num_cls1_valid_rows = {}".format(num_cls1_valid_rows))
+    valid_cls1 = numpy.array(f['DATA/DATA'])
+    valid_cls1_lbl = numpy.ones(num_cls1_valid_rows, dtype=int)
+
+    print("Reading Class 1 Testing")
+    f = h5py.File(cls1_test_file, 'r')
+    num_cls1_test_rows = f['DATA/DATA'].shape[0]
+    print("num_cls1_test_rows = {}".format(num_cls1_test_rows))
+    test_cls1 = numpy.array(f['DATA/DATA'])
+    test_cls1_lbl = numpy.ones(num_cls1_test_rows, dtype=int)
+
+    print("Reading Class 2 Training")
+    f = h5py.File(cls2_train_file, 'r')
+    num_cls2_train_rows = f['DATA/DATA'].shape[0]
+    print("num_cls2_train_rows = {}".format(num_cls2_train_rows))
+    train_cls2 = numpy.array(f['DATA/DATA'])
+    train_cls2_lbl = numpy.zeros(num_cls2_train_rows, dtype=int)
+
+    print("Reading Class 2 Validation")
+    f = h5py.File(cls2_valid_file, 'r')
+    num_cls2_valid_rows = f['DATA/DATA'].shape[0]
+    print("num_cls2_valid_rows = {}".format(num_cls2_valid_rows))
+    valid_cls2 = numpy.array(f['DATA/DATA'])
+    valid_cls2_lbl = numpy.zeros(num_cls2_valid_rows, dtype=int)
+
+    print("Reading Class 2 Testing")
+    f = h5py.File(cls2_test_file, 'r')
+    num_cls2_test_rows = f['DATA/DATA'].shape[0]
+    print("num_cls2_test_rows = {}".format(num_cls2_test_rows))
+    test_cls2 = numpy.array(f['DATA/DATA'])
+    test_cls2_lbl = numpy.zeros(num_cls2_test_rows, dtype=int)
+
+    print("Finished Reading Data")
+
+    vaild_np = numpy.concatenate((valid_cls2, valid_cls1))
+    vaild_lbl_np = numpy.concatenate((valid_cls2_lbl, valid_cls1_lbl))
+    d_valid = xgb.DMatrix(vaild_np, label=vaild_lbl_np)
+
+    d_train = xgb.DMatrix(numpy.concatenate((train_cls2, train_cls1)),
+                          label=numpy.concatenate((train_cls2_lbl, train_cls1_lbl)))
+
+    test_np = numpy.concatenate((test_cls2, test_cls1))
+    test_lbl_np = numpy.concatenate((test_cls2_lbl, test_cls1_lbl))
+    d_test = xgb.DMatrix(test_np, label=test_lbl_np)
+
+    with open(cls_params_file) as f:
+        cls_params = json.load(f)
+
+    print("Start Training Find Classifier")
+
+    params = {'eta'             : cls_params['eta'],
+              'gamma'           : cls_params['gamma'],
+              'max_depth'       : cls_params['max_depth'],
+              'min_child_weight': cls_params['min_child_weight'],
+              'max_delta_step'  : cls_params['max_delta_step'],
+              'subsample'       : cls_params['subsample'],
+              'nthread'         : nthread,
+              'eval_metric'     : cls_params['eval_metric'],
+              'objective'       : cls_params['objective']}
+
+    num_boost_round = cls_params['num_boost_round']
+
+    evals_results = {}
+    watchlist = [(d_train, 'train'), (d_valid, 'validation')]
+    model = xgb.train(params, d_train, num_boost_round, evals=watchlist, evals_result=evals_results,
+                      verbose_eval=False, xgb_model=mdl_cls_obj)
+    test_auc = roc_auc_score(test_lbl_np, model.predict(d_test))
+    print("Testing AUC: {}".format(test_auc))
+    print("Finish Training")
+
+    model.save_model(out_mdl_file)
+
+    pred_test = model.predict(d_test)
+    for i in range(test_np.shape[0]):
+        if (pred_test[i] >= 0.5):
+            pred_test[i] = 1
+        else:
+            pred_test[i] = 0
+    len(pred_test)
+
+    test_acc = accuracy_score(test_lbl_np, pred_test)
+    print("Testing Accuracy: {}".format(test_acc))
+
+
+def train_opt_xgboost_binary_classifer(out_mdl_file, cls1_train_file, cls1_valid_file, cls1_test_file,
+                                       cls2_train_file, cls2_valid_file, cls2_test_file, nthread=2,
+                                       scale_pos_weight=None, mdl_cls_obj=None, out_params_file=None):
     """
     A function which performs a bayesian optimisation of the hyper-parameters for a binary xgboost
     classifier. Class 1 is the class which you are interested in and Class 2 is the 'other class'.
@@ -80,6 +318,8 @@ def train_xgboost_binary_classifer(out_mdl_file, cls1_train_file, cls1_valid_fil
     :param scale_pos_weight: Optional, default is None. If None then a value will automatically be calculated.
                              Parameter used to balance imbalanced training data.
     :param mdl_cls_obj: XGBoost object to allow continue training with a new dataset.
+    :param out_params_file: The output model parameters which have been optimised.
+                            If None then no file will be outputted.
 
     """
     print("Reading Class 1 Training")
@@ -185,6 +425,21 @@ def train_xgboost_binary_classifer(out_mdl_file, cls1_train_file, cls1_valid_fil
     best_params = res_gp.x
 
     print("Best Params:\n{}".format(best_params))
+
+    if out_params_file is not None:
+        out_params = {'eta'             : float(best_params[0]),
+                      'gamma'           : int(best_params[1]),
+                      'max_depth'       : int(best_params[2]),
+                      'min_child_weight': int(best_params[3]),
+                      'max_delta_step'  : int(best_params[4]),
+                      'subsample'       : float(best_params[5]),
+                      'nthread'         : int(nthread),
+                      'eval_metric'     : 'auc',
+                      'objective'       : 'binary:logistic',
+                      'num_boost_round' : int(best_params[6])}
+
+        with open(out_params_file, 'w') as fp:
+            json.dump(out_params, fp, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
 
     print("Start Training Find Classifier")
 
