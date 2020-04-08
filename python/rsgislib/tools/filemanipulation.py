9fcf9bca37a9f126af53903c09e00d2b4889c3b8
Pete Bunting <petebunting@mac.com>
2020-04-08 17:10:01 +0100

    Update to attempt to fix cmake python installation issue on windows. Also added functions and cmd utility to calculate file hash.

--- python/rsgislib/tools/filemanipulation.py
+++ python/rsgislib/tools/filemanipulation.py
@@ -39,6 +39,207 @@ Where:
                 shutil.move(tmpFile, outFile)
 
 
+def createSHA1Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA1 hash string of the input file.
+    :param input_file: the input file for which the SHA1 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA1 hash string of the file.
 
+    """
+    import hashlib
+    sha1_hash = hashlib.sha1()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha1_hash.update(byte_block)
+    return sha1_hash.hexdigest()
+
+
+def createSHA224Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA224 hash string of the input file.
+    :param input_file: the input file for which the SHA224 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA224 hash string of the file.
+
+    """
+    import hashlib
+    sha224_hash = hashlib.sha224()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha224_hash.update(byte_block)
+    return sha224_hash.hexdigest()
+
+
+def createSHA256Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA256 hash string of the input file.
+    :param input_file: the input file for which the SHA256 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA256 hash string of the file.
+
+    """
+    import hashlib
+    sha256_hash = hashlib.sha256()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+
+def createSHA384Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA384 hash string of the input file.
+    :param input_file: the input file for which the SHA384 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA384 hash string of the file.
+
+    """
+    import hashlib
+    sha384_hash = hashlib.sha384()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha384_hash.update(byte_block)
+    return sha384_hash.hexdigest()
+
+
+def createSHA512Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA512 hash string of the input file.
+    :param input_file: the input file for which the SHA512 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA512 hash string of the file.
+
+    """
+    import hashlib
+    sha512_hash = hashlib.sha512()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha512_hash.update(byte_block)
+    return sha512_hash.hexdigest()
+
+
+def createMD5Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the MD5 hash string of the input file.
+    :param input_file: the input file for which the MD5 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: MD5 hash string of the file.
+
+    """
+    import hashlib
+    md5_hash = hashlib.md5()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            md5_hash.update(byte_block)
+    return md5_hash.hexdigest()
+
+
+def createBlake2BHash(input_file, block_size=4096):
+    """
+    A function which calculates finds the Blake2B hash string of the input file.
+    :param input_file: the input file for which the Blake2B hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: Blake2B hash string of the file.
+
+    """
+    import hashlib
+    blake2b_hash = hashlib.blake2b()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            blake2b_hash.update(byte_block)
+    return blake2b_hash.hexdigest()
+
+
+def createBlake2SHash(input_file, block_size=4096):
+    """
+    A function which calculates finds the Blake2S hash string of the input file.
+    :param input_file: the input file for which the Blake2S hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: Blake2S hash string of the file.
+
+    """
+    import hashlib
+    blake2s_hash = hashlib.blake2s()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            blake2s_hash.update(byte_block)
+    return blake2s_hash.hexdigest()
+
+
+def createSHA3_224Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA3_224 hash string of the input file.
+    :param input_file: the input file for which the SHA3_224 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA3_224 hash string of the file.
+
+    """
+    import hashlib
+    sha224_hash = hashlib.sha3_224()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha224_hash.update(byte_block)
+    return sha224_hash.hexdigest()
+
+
+def createSHA3_256Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA3_256 hash string of the input file.
+    :param input_file: the input file for which the SHA3_256 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA3_256 hash string of the file.
+
+    """
+    import hashlib
+    sha256_hash = hashlib.sha3_256()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+
+def createSHA3_384Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA3_384 hash string of the input file.
+    :param input_file: the input file for which the SHA3_384 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA3_384 hash string of the file.
+
+    """
+    import hashlib
+    sha384_hash = hashlib.sha3_384()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha384_hash.update(byte_block)
+    return sha384_hash.hexdigest()
+
+
+def createSHA3_512Hash(input_file, block_size=4096):
+    """
+    A function which calculates finds the SHA3_512 hash string of the input file.
+    :param input_file: the input file for which the SHA3_512 hash string with be found.
+    :param block_size: the size of the blocks the file is read in in bytes (default 4096; i.e., 4kb)
+    :return: SHA3_512 hash string of the file.
+
+    """
+    import hashlib
+    sha512_hash = hashlib.sha3_512()
+    with open(input_file, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(block_size), b""):
+            sha512_hash.update(byte_block)
+    return sha512_hash.hexdigest()
 
 
