0328ceb3a6e341d0485e700d994ccb4307fd24ee
Pete Bunting <petebunting@mac.com>
2020-08-17 08:47:19 +0100

    Add function to create numeric columns based off an existing column

--- python/rsgislib/vectorutils/__init__.py
+++ python/rsgislib/vectorutils/__init__.py
@@ -5897,4 +5897,66 @@ def explode_vec_files(input_vecs, output_dir, out_format='GPKG', out_file_ext='g
             explode_vec_lyr(vec_file, lyr, out_vec_file, lyr, out_format)
 
 
+def add_unq_numeric_col(vec_file, vec_lyr, unq_col, out_col, vec_out_file, vec_out_lyr, out_format='GPKG'):
+    """
+    A function which adds a numeric column based off an existing column in the vector file.
+
+    :param vec_file: Input vector file.
+    :param vec_lyr: Input vector layer within the input file.
+    :param unq_col: The column within which the unique values will be identified.
+    :param out_col: The output numeric column
+    :param vec_out_file: Output vector file
+    :param vec_out_lyr: output vector layer name.
+    :param out_format: output file format (default GPKG).
+
+    """
+    import geopandas
+    import numpy
+
+    base_gpdf = geopandas.read_file(vec_file, layer=vec_lyr)
+    unq_vals = base_gpdf[unq_col].unique()
+
+    base_gpdf[out_col] = numpy.zeros((base_gpdf.shape[0]), dtype=int)
+    num_unq_val = 1
+    for unq_val in unq_vals:
+        sel_rows = base_gpdf[unq_col] == unq_val
+        base_gpdf.loc[sel_rows, out_col] = num_unq_val
+        num_unq_val += 1
+
+    if out_format == 'GPKG':
+        base_gpdf.to_file(vec_out_file, layer=vec_out_lyr, driver=out_format)
+    else:
+        base_gpdf.to_file(vec_out_file, driver=out_format)
+
+
+def add_numeric_col_lut(vec_file, vec_lyr, ref_col, val_lut, out_col, vec_out_file, vec_out_lyr, out_format='GPKG'):
+    """
+        A function which adds a numeric column based off an existing column in the vector file,
+        using an dict LUT to define the values.
+
+        :param vec_file: Input vector file.
+        :param vec_lyr: Input vector layer within the input file.
+        :param ref_col: The column within which the unique values will be identified.
+        :param val_lut: A dict LUT (key should be value in ref_col and value be the value outputted to out_col).
+        :param out_col: The output numeric column
+        :param vec_out_file: Output vector file
+        :param vec_out_lyr: output vector layer name.
+        :param out_format: output file format (default GPKG).
+
+        """
+    import geopandas
+    import numpy
+    # Open vector file
+    base_gpdf = geopandas.read_file(vec_file, layer=vec_lyr)
+    # Add output column
+    base_gpdf[out_col] = numpy.zeros((base_gpdf.shape[0]), dtype=int)
+    # Loop values in LUT
+    for lut_key in val_lut:
+        sel_rows = base_gpdf[ref_col] == lut_key
+        base_gpdf.loc[sel_rows, out_col] = val_lut[lut_key]
+
+    if out_format == 'GPKG':
+        base_gpdf.to_file(vec_out_file, layer=vec_out_lyr, driver=out_format)
+    else:
+        base_gpdf.to_file(vec_out_file, driver=out_format)
 
