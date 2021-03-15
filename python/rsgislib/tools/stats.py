5ec97d22862d274bc88b67e72b56589944fb6cc5
Pete Bunting <petebunting@mac.com>
2021-03-15 09:17:46 +0000

    Update to stats tools

--- python/rsgislib/tools/stats.py
+++ python/rsgislib/tools/stats.py
@@ -4,67 +4,134 @@ The tools.stats module contains some useful tools for calculating useful statist
 easily available else where.
 """
 
-def calculate_VIF(X):
+
+def calc_pandas_VIF(df, cols=None):
     """
     A function to calculate variance inflation factors to
-    investigate multicollinearity between predictor variables. 
-    Function written by Osian Roberts.
+    investigate multicollinearity between predictor variables.
 
-    :param X: a 2D numpy array of shape = (nSamples, nPredictors).
-    :returns: VIF_scores; A numpy array containing the VIF for each predictor variable.
-
-    Interpretation of VIF scores (this is somewhat subjective):
+    Interpretation of VIF scores (somewhat subjective):
     1 = No multicollinearity.
     1-5 = Moderate multicollinearity.
     > 5 = High multicollinearity.
     > 10 = This predictor should be removed from the model.
-    
+
+    :param df: pandas dataframe where the columns are the predictor variables
+    :param cols: list of columns in the dataframe
+    :returns: A pandas series containing the VIF for each predictor variable.
+
     Example::
-    
+        df = pandas.read_csv('metrics.csv')
+        cols = list(df.columns)
+        # Subset to the column names of interest
+        ind_vars = cols[6:]
+        vifs_series = calc_pandas_VIF(df, ind_vars)
+        vifs_series.to_csv('VIF_scores.csv')
+
+    """
+    # Import the linear model module.
+    import sklearn.linear_model
     import numpy
-    # create some fake predictors to test the function:
-    x1 = numpy.random.uniform(1, 100, 500)  # this should be highly collinear with x3.
-    x2 = numpy.random.uniform(1, 100, 500)  # this should be independent of x1.
-    x3 = 0.85 * x1 + 50 # this should be highly collinear with x1.
-    x4 = 0.85 * numpy.log10(x1) + 50 # this should be moderately collinear with x1.
-    
-    X = numpy.array([x1, x2, x3, x4]).T
-    
-    vif_scores = calculate_VIF(X)
-        
-    for i in range(vif_scores.size):
-        print('VIF for x' + str(i + 1) + ':', vif_scores[i])
-    
+    import pandas
+
+    # If the cols is None then get a list of all the columns
+    if cols is None:
+        cols = list(df.columns)
+
+    # If there is less than 2 columns product error message.
+    if len(cols) < 2:
+        raise Exception('The list of columns must be have a length of at least 2.')
+
+    print('Calculating VIF for {} predictors variables...'.format(len(cols)))
+
+    # Create a linear model instance.
+    lm = sklearn.linear_model.LinearRegression()
+
+    # Create the dict for the output scores
+    vif_scores = dict()
+    # Iterative through the columns.
+    for col in cols:
+        # Get the list of columns without the current column
+        tmp_cols = cols.copy()
+        tmp_cols.remove(col)
+        # Create numpy array for the y value to be fitted.
+        y = df[col].values
+        # Create numpy array for the x values
+        x = df[tmp_cols].values
+        lm.fit(x, y)
+        # Use fitted model to predict y values
+        y_pred = lm.predict(x)
+
+        # Calculate the coefficient of determination:
+        ss_tot = sum((y - numpy.mean(y)) ** 2)
+        ss_res = sum((y - y_pred) ** 2)
+        r2 = 1 - (ss_res / ss_tot)
+
+        # Calculate VIF score:
+        if r2 == 1:
+            vif_scores[col] = 0.0
+        else:
+            vif_scores[col] = (1 / (1 - r2))
+
+    # Create a pandas series and return it.
+    return pandas.Series(vif_scores, name='VIF')
+
+
+def cqv_threshold(df, cols=None, lowthreshold=0.25, highthreshold=0.75):
+    """
+    A function to remove features with low & high variance based on the coefficient
+    of quartile variation (CQV).
+
+    Low CQV == uninformative predictor.
+    High CQV == unstable predictor variable.
+
+    Inspired by sklearn.feature_selection.VarianceThreshold(), but more robust
+    since the coefficient of quartile variation is independent of feature scaling.
+    It is also less susceptible to outliers than the coefficient of variation.
+
+    References:
+    https://en.wikipedia.org/wiki/Quartile_coefficient_of_dispersion
+    https://cran.r-project.org/web/packages/cvcqv/vignettes/cqv_versatile.html
+
+    :param df: pandas dataframe where the columns are the predictor variables
+    :param cols: list of columns in the dataframe
+    :param lowthreshold: Float defining the CQV below which the predictors are uninfomative
+    :param highthreshold: Float defining the CQV above which the predictors are unreliable
+    :returns: list of column names for good predictor variables
+
     """
     import numpy
-    from sklearn.linear_model import LinearRegression
-    
-    print('Calculating VIF for {} predictors variables...'.format(X.shape[1]))
-    # fit auxiliary regressions to each predictor in X:
-    lm = LinearRegression()
-    VIF_scores = []
-    for i in range(X.shape[1]):
-        # fit an OLS linear model to each predictor (y) using the remaining predictor variables (x):
-        y = X[:, i]
-        x = numpy.delete(X, i, axis=1)
-        lm.fit(x, y)
-        y_pred = lm.predict(x)
-        del x
 
-        # calculate the coefficient of determination:
-        ss_tot = sum((y - numpy.mean(y))**2)
-        ss_res = sum((y - y_pred)**2)
-        r2 = 1 - (ss_res / ss_tot)
-        del y, y_pred, ss_res, ss_tot
+    # If the cols is None then get a list of all the columns
+    if cols is None:
+        cols = list(df.columns)
 
-        # calculate VIF:
-        if r2 == 1:
-            r2 = 0.999  # this prevents division by zero errors when r2 == 1.
-        VIF_scores.append(1 / (1 - r2))
-        del r2
-    del lm, X
-    VIF_scores = numpy.array(VIF_scores).round(decimals=1)
-    return VIF_scores
+    # If there is less than 2 columns product error message.
+    if len(cols) < 2:
+        raise Exception('The list of columns must be have a length of at least 2.')
+
+    # Create numpy array from the list columns
+    x = df[cols].values
+
+    # Calculate the Coefficient of Variation:
+    print('Calculating CQV for {} predictor variables...'.format(len(cols)))
+    q1 = numpy.percentile(x, 25, axis=0)
+    q3 = numpy.percentile(x, 75, axis=0)
+    cqv = (q3 - q1) / (q3 + q1)
+
+    print('Median CQV: {}'.format(numpy.median(cqv)))
+
+    # Index the good predictors:
+    good_idx = numpy.where((cqv >= lowthreshold) & (cqv <= highthreshold))[0]
+
+    # Get a list of column names
+    good_cols = []
+    for i in good_idx:
+        good_cols.append(cols[i])
+
+    print('Selected {} useful predictors...'.format(len(good_cols)))
+
+    return good_cols
 
 
 def bin_accuracy_scores_prob(y_true, y_prob):
@@ -74,11 +141,11 @@ def bin_accuracy_scores_prob(y_true, y_prob):
 
     :param y_true: binary class labels, where 0 is absence and 1 is presence.
     :param y_prob: probability of presence scores e.g., generated by a species distribution model.
-
     :returns: a list containing two arrays - metrics = names of test metrics. scores = test scores for each metric.
 
     Useful reference:
     https://machinelearningmastery.com/how-to-score-probability-predictions-in-python
+
     """
     import numpy
     # check inputs:
@@ -130,19 +197,19 @@ def bin_accuracy_scores_prob(y_true, y_prob):
 
 
 def accuracy_scores_binary(y_true, y_pred):
-    '''
+    """
     A function to calculate accuracy measures for a binary classification.
     Function written by Osian Roberts.
 
     Parameters:
     :param y_true: observed binary labels, where 0 is absence and 1 is presence.
     :param y_pred: predicted binary labels, where 0 is absence and 1 is presence.
-
     :returns: a list containing two numpy.arrays - (metrics: name of test metrics, scores: test scores for each metric)
 
     Reference: See pages 253 - 255 in:
     Guisan et al. (2017). Habitat suitability and distribution models: with applications in R.
-    '''
+    
+    """
     import numpy
 
     # check inputs:
