d55ee7e10c997893edc70c5f2ce0d7a3bfbe1a42
Pete Bunting <petebunting@mac.com>
2021-05-06 14:12:19 +0100

    Add functions and tool check images are valid.

--- python/rsgislib/__init__.py
+++ python/rsgislib/__init__.py
@@ -166,6 +166,7 @@ SHARP_RES_IGNORE = 0
 SHARP_RES_LOW = 1
 SHARP_RES_HIGH = 2
 
+
 def getRSGISLibVersion():
     """ Calls rsgis-config to get the version number. """
 
@@ -185,6 +186,7 @@ __version__ = getRSGISLibVersion()
 py_sys_version_str = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
 py_sys_version_flt = float(py_sys_version_str)
 
+
 class RSGISPyException(Exception):
     """
     A class representing the RSGIS exception.
@@ -201,6 +203,37 @@ class RSGISPyException(Exception):
         Return a string representation of the exception
         """
         return repr(self.value)
+
+
+class RSGISGDALErrorHandler(object):
+    """
+    A class representing a generic GDAL Error Handler which
+    can be used to pick up GDAL warnings rather than just
+    failure errors.
+    """
+
+    def __init__(self):
+        """
+        Init for RSGISGDALErrorHandler. Class attributes are err_level, err_no and err_msg
+
+        """
+        from osgeo import gdal
+        self.err_level = gdal.CE_None
+        self.err_no = 0
+        self.err_msg = ''
+
+    def handler(self, err_level, err_no, err_msg):
+        """
+        The handler function which is called with the error information.
+
+        :param err_level: The level of the error
+        :param err_no: The error number
+        :param err_msg: The message (string) associated with the error.
+
+        """
+        self.err_level = err_level
+        self.err_no = err_no
+        self.err_msg = err_msg
 
 
 class RSGISPyUtils (object):
@@ -2059,6 +2092,85 @@ class RSGISPyUtils (object):
             raise Exception("The year specified is before the base date.")
         date_val = datetime.date(year=int(year), month=int(month), day=int(day))
         return (date_val - base_date).days
+
+    def check_gdal_image_file(self, gdal_img):
+        """
+        A function which checks a GDAL compatible image file and returns an error message if appropriate.
+
+        :param gdal_img: the file path to the gdal image file.
+        :return: boolean (True: file OK; False: Error found), string (error message if required otherwise empty string)
+
+        """
+        file_ok = True
+        err_str = ''
+        if os.path.exists(gdal_img):
+            err = RSGISGDALErrorHandler()
+            err_handler = err.handler
+            gdal.PushErrorHandler(err_handler)
+            gdal.UseExceptions()
+
+            try:
+                raster_ds = gdal.Open(gdal_img, gdal.GA_ReadOnly)
+                if raster_ds is None:
+                    file_ok = False
+                    err_str = 'GDAL could not open the dataset, returned None.'
+                raster_ds = None
+            except Exception as e:
+                file_ok = False
+                err_str = str(e)
+            else:
+                if err.err_level >= gdal.CE_Warning:
+                    file_ok = False
+                    err_str = str(err.err_msg)
+            finally:
+                gdal.PopErrorHandler()
+        else:
+            file_ok = False
+            err_str = 'File does not exist.'
+        return file_ok, err_str
+
+    def check_gdal_vector_file(self, gdal_vec):
+        """
+        A function which checks a GDAL compatible vector file and returns an error message if appropriate.
+
+        :param gdal_vec: the file path to the gdal vector file.
+        :return: boolean (True: file OK; False: Error found), string (error message if required otherwise empty string)
+
+        """
+        file_ok = True
+        err_str = ''
+        if os.path.exists(gdal_vec):
+            err = RSGISGDALErrorHandler()
+            err_handler = err.handler
+            gdal.PushErrorHandler(err_handler)
+            gdal.UseExceptions()
+
+            try:
+                vec_ds = gdal.OpenEx(gdal_vec, gdal.OF_VECTOR)
+                if vec_ds is None:
+                    file_ok = False
+                    err_str = 'GDAL could not open the data source, returned None.'
+                else:
+                    for lyr_idx in range(vec_ds.GetLayerCount()):
+                        vec_lyr = vec_ds.GetLayerByIndex(lyr_idx)
+                        if vec_lyr is None:
+                            file_ok = False
+                            err_str = 'GDAL could not open all the vector layers.'
+                            break
+                vec_ds = None
+            except Exception as e:
+                file_ok = False
+                err_str = str(e)
+            else:
+                if err.err_level >= gdal.CE_Warning:
+                    file_ok = False
+                    err_str = str(err.err_msg)
+            finally:
+                gdal.PopErrorHandler()
+        else:
+            file_ok = False
+            err_str = 'File does not exist.'
+        return file_ok, err_str
 
 
 class RSGISTime (object):
