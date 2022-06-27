0970beff2e32c10cd9b312fd6ec3a24d1bd2f86e
Pete Bunting <petebunting@mac.com>
2022-06-27 14:29:56 +0100

    Update to mapping module removing function as same functionality is available by combining functions.

--- python/rsgislib/tools/mapping.py
+++ python/rsgislib/tools/mapping.py
@@ -129,14 +129,15 @@ def add_overview_maps(
 
 def create_vec_lyr_map(
     ax: plt.axis,
-    gp_vec: geopandas.GeoDataFrame,
+    gp_vecs: Union[geopandas.GeoDataFrame, List[geopandas.GeoDataFrame]],
     bbox: List[float],
     title_str: str = None,
     cx_src=None,
-    vec_fill_clr: str = "grey",
-    vec_line_clr: str = "black",
-    vec_line_width: float = 0.25,
-    vec_fill_alpha: int = 1,
+    vec_fill_clrs: Union[str, List[str]] = "grey",
+    vec_line_clrs: Union[str, List[str]] = "black",
+    vec_line_widths: Union[float, List[float]] = 0.25,
+    vec_fill_alphas: Union[float, List[float]] = 1.0,
+    show_scale_bar: bool = True,
     use_grid: bool = False,
     show_map_axis: bool = True,
     sub_in_vec: bool = False,
@@ -147,14 +148,15 @@ def create_vec_lyr_map(
     """
 
     :param ax:
-    :param gp_vec:
+    :param gp_vecs:
     :param bbox:
     :param title_str:
     :param cx_src:
-    :param vec_fill_clr:
-    :param vec_line_clr:
-    :param vec_line_width:
-    :param vec_fill_alpha:
+    :param vec_fill_clrs:
+    :param vec_line_clrs:
+    :param vec_line_widths:
+    :param vec_fill_alphas:
+    :param show_scale_bar:
     :param use_grid:
     :param show_map_axis:
     :param sub_in_vec:
@@ -166,22 +168,66 @@ def create_vec_lyr_map(
     if cx_src is not None:
         import contextily
 
-    if sub_in_vec:
-        min_x_sub = math.floor(bbox[0] - 4)
-        max_x_sub = math.ceil(bbox[1] + 4)
-        min_y_sub = math.floor(bbox[2] - 4)
-        max_y_sub = math.ceil(bbox[3] + 4)
-        gp_vec_sub = gp_vec.cx[min_x_sub:max_x_sub, min_y_sub:max_y_sub]
+    n_vec_lyrs = 1
+    if type(gp_vecs) is list:
+        n_vec_lyrs = len(gp_vecs)
     else:
-        gp_vec_sub = gp_vec
+        # Create list of one
+        gp_vecs = [gp_vecs]
 
-    gp_vec_sub.plot(
-        ax=ax,
-        color=vec_fill_clr,
-        edgecolor=vec_line_clr,
-        linewidth=vec_line_width,
-        alpha=vec_fill_alpha,
-    )
+    for i, gp_vec in enumerate(gp_vecs):
+        if sub_in_vec:
+            min_x_sub = math.floor(bbox[0] - 4)
+            max_x_sub = math.ceil(bbox[1] + 4)
+            min_y_sub = math.floor(bbox[2] - 4)
+            max_y_sub = math.ceil(bbox[3] + 4)
+            gp_vec_sub = gp_vec.cx[min_x_sub:max_x_sub, min_y_sub:max_y_sub]
+        else:
+            gp_vec_sub = gp_vec
+
+        if type(vec_fill_clrs) is list:
+            if len(vec_fill_clrs) != n_vec_lyrs:
+                raise rsgislib.RSGISPyException(
+                    "Number of fill colours not same as number of vector layers"
+                )
+            vec_fill_clr = vec_fill_clrs[i]
+        else:
+            vec_fill_clr = vec_fill_clrs
+
+        if type(vec_line_clrs) is list:
+            if len(vec_fill_clrs) != n_vec_lyrs:
+                raise rsgislib.RSGISPyException(
+                    "Number of line colours not same as number of vector layers"
+                )
+            vec_line_clr = vec_line_clrs[i]
+        else:
+            vec_line_clr = vec_line_clrs
+
+        if type(vec_line_widths) is list:
+            if len(vec_fill_clrs) != n_vec_lyrs:
+                raise rsgislib.RSGISPyException(
+                    "Number of line widths not same as number of vector layers"
+                )
+            vec_line_width = vec_line_widths[i]
+        else:
+            vec_line_width = vec_line_widths
+
+        if type(vec_fill_alphas) is list:
+            if len(vec_fill_clrs) != n_vec_lyrs:
+                raise rsgislib.RSGISPyException(
+                    "Number of fill alphas not same as number of vector layers"
+                )
+            vec_fill_alpha = vec_fill_alphas[i]
+        else:
+            vec_fill_alpha = vec_fill_alphas
+
+        gp_vec_sub.plot(
+            ax=ax,
+            color=vec_fill_clr,
+            edgecolor=vec_line_clr,
+            linewidth=vec_line_width,
+            alpha=vec_fill_alpha,
+        )
     ax.set_xlim([bbox[0], bbox[1]])
     ax.set_ylim([bbox[2], bbox[3]])
 
@@ -189,7 +235,7 @@ def create_vec_lyr_map(
         contextily.add_basemap(
             ax,
             zoom=cx_zoom_lvl,
-            crs=gp_vec_sub.crs,
+            crs=gp_vecs[0].crs,
             source=cx_src,
             attribution=cx_attribution,
             attribution_size=cx_att_size,
@@ -202,10 +248,11 @@ def create_vec_lyr_map(
         ax.set_axis_off()
         ax.set_axis_off()
 
-    distance_meters = rsgislib.tools.projection.great_circle_distance(
-        wgs84_p1=[bbox[0], bbox[3]], wgs84_p2=[bbox[0] + 1, bbox[3]]
-    )
-    ax.add_artist(ScaleBar(distance_meters))
+    if show_scale_bar:
+        distance_meters = rsgislib.tools.projection.great_circle_distance(
+            wgs84_p1=[bbox[0], bbox[3]], wgs84_p2=[bbox[0] + 1, bbox[3]]
+        )
+        ax.add_artist(ScaleBar(distance_meters))
 
     if title_str is not None:
         ax.title.set_text(title_str)
@@ -218,6 +265,7 @@ def create_raster_img_map(
     img_stch: int,
     bbox=None,
     title_str=None,
+    show_scale_bar: bool = True,
     use_grid: bool = False,
     show_map_axis: bool = True,
     img_no_data_val=None,
@@ -232,6 +280,7 @@ def create_raster_img_map(
     :param img_stch:
     :param bbox:
     :param title_str:
+    :param show_scale_bar:
     :param use_grid:
     :param show_map_axis:
     :param img_no_data_val:
@@ -284,10 +333,11 @@ def create_raster_img_map(
         ax.set_axis_off()
         ax.set_axis_off()
 
-    distance_meters = rsgislib.tools.projection.great_circle_distance(
-        wgs84_p1=[bbox[0], bbox[3]], wgs84_p2=[bbox[0] + 1, bbox[3]]
-    )
-    ax.add_artist(ScaleBar(distance_meters))
+    if show_scale_bar:
+        distance_meters = rsgislib.tools.projection.great_circle_distance(
+            wgs84_p1=[bbox[0], bbox[3]], wgs84_p2=[bbox[0] + 1, bbox[3]]
+        )
+        ax.add_artist(ScaleBar(distance_meters))
 
     if title_str is not None:
         ax.title.set_text(title_str)
@@ -300,6 +350,7 @@ def create_thematic_raster_map(
     title_str: str = None,
     cx_src=None,
     alpha_backgd: bool = True,
+    show_scale_bar: bool = True,
     use_grid: bool = False,
     show_map_axis: bool = True,
     cx_zoom_lvl: int = Union[int, "auto"],
@@ -314,6 +365,7 @@ def create_thematic_raster_map(
     :param title_str:
     :param cx_src:
     :param alpha_backgd:
+    :param show_scale_bar:
     :param use_grid:
     :param show_map_axis:
     :param cx_zoom_lvl:
@@ -372,122 +424,11 @@ def create_thematic_raster_map(
         ax.set_axis_off()
         ax.set_axis_off()
 
-    distance_meters = rsgislib.tools.projection.great_circle_distance(
-        wgs84_p1=[bbox[0], bbox[3]], wgs84_p2=[bbox[0] + 1, bbox[3]]
-    )
-    ax.add_artist(ScaleBar(distance_meters))
-
-    if title_str is not None:
-        ax.title.set_text(title_str)
-
-
-def create_thematic_raster_img_base_map(
-    ax: plt.axis,
-    in_base_img: str,
-    img_bands: List[int],
-    img_stch: int,
-    in_them_img: Union[str, List],
-    bbox: List[float] = None,
-    title_str: str = None,
-    alpha_backgd: bool = True,
-    use_grid: bool = False,
-    show_map_axis: bool = True,
-    img_no_data_val=None,
-    stch_min_max_vals=None,
-    stch_n_stdevs=2.0,
-):
-    """
-
-    :param ax:
-    :param in_base_img:
-    :param img_bands:
-    :param img_stch:
-    :param in_them_img:
-    :param bbox:
-    :param title_str:
-    :param alpha_backgd:
-    :param use_grid:
-    :param show_map_axis:
-    :param img_no_data_val:
-    :param stch_min_max_vals:
-    :param stch_n_stdevs:
-    :return:
-    """
-    if img_no_data_val is None:
-        img_no_data_val = rsgislib.imageutils.get_img_no_data_value(in_base_img)
-
-    if bbox is None:
-        bbox = rsgislib.imageutils.get_img_bbox(in_base_img)
-
-    in_them_imgs = list()
-    if isinstance(in_them_img, str):
-        in_them_imgs.append(in_them_img)
-    else:
-        in_them_imgs = in_them_img
-
-    img_data_arr_scns = list()
-    img_coords_scns = list()
-    lgd_patches_scns = list()
-    for in_them_img in in_them_imgs:
-        (
-            img_data_arr,
-            img_coords,
-            lgd_patches,
-        ) = rsgislib.tools.plotting.get_gdal_thematic_raster_mpl_imshow(
-            in_them_img, band=1, bbox=bbox, alpha_lyr=alpha_backgd
+    if show_scale_bar:
+        distance_meters = rsgislib.tools.projection.great_circle_distance(
+            wgs84_p1=[bbox[0], bbox[3]], wgs84_p2=[bbox[0] + 1, bbox[3]]
         )
-        img_data_arr_scns.append(img_data_arr)
-        img_coords_scns.append(img_coords)
-        lgd_patches_scns.append(lgd_patches)
-
-    img_data, img_coords = rsgislib.tools.plotting.get_gdal_raster_mpl_imshow(
-        in_base_img, bands=img_bands, bbox=bbox
-    )
-
-    if img_stch == rsgislib.IMG_STRETCH_USER:
-        if stch_min_max_vals is None:
-            raise rsgislib.RSGISPyException(
-                "Manual stretch requires user parameters to be passed"
-            )
-        img_data_strch = rsgislib.tools.plotting.manual_stretch_np_arr(
-            img_data, min_max_vals=stch_min_max_vals, no_data_val=img_no_data_val
-        )
-    elif img_stch == rsgislib.IMG_STRETCH_LINEAR:
-        img_data_strch = rsgislib.tools.plotting.linear_stretch_np_arr(
-            img_data, no_data_val=img_no_data_val
-        )
-    elif img_stch == rsgislib.IMG_STRETCH_STDEV:
-        img_data_strch = rsgislib.tools.plotting.stdev_stretch_np_arr(
-            img_data, n_stdevs=stch_n_stdevs, no_data_val=img_no_data_val
-        )
-    elif img_stch == rsgislib.IMG_STRECTH_CUMULATIVE:
-        img_data_strch = rsgislib.tools.plotting.cumulative_stretch_np_arr(
-            img_data, no_data_val=img_no_data_val
-        )
-    else:
-        print("No stretch is being used - is this what you intended?!")
-        img_data_strch = img_data
-
-    # base image data
-    ax.imshow(img_data_strch, extent=img_coords)
-    ax.set_xlim([img_coords[0], img_coords[1]])
-    ax.set_ylim([img_coords[2], img_coords[3]])
-
-    # thematic data
-    for img_data_arr, img_therm_coords in zip(img_data_arr_scns, img_coords_scns):
-        ax.imshow(img_data_arr, extent=img_therm_coords)
-
-    if use_grid:
-        ax.grid()
-
-    if not show_map_axis:
-        ax.set_axis_off()
-        ax.set_axis_off()
-
-    distance_meters = rsgislib.tools.projection.great_circle_distance(
-        wgs84_p1=[bbox[0], bbox[3]], wgs84_p2=[bbox[0] + 1, bbox[3]]
-    )
-    ax.add_artist(ScaleBar(distance_meters))
+        ax.add_artist(ScaleBar(distance_meters))
 
     if title_str is not None:
         ax.title.set_text(title_str)
