3e1b23b09d23f06302f835d9ba81039dc2f617ad
Pete Bunting <petebunting@mac.com>
2024-01-04 23:18:50 +0000

    Update script for running sonar-scanner.

--- run_sonar_scanner.sh
+++ run_sonar_scanner.sh
@@ -1,5 +1,13 @@
+mkdir -p pytest_reports/coverage
+
+#pytest -v python_tests --cov=python/rsgislib --cov-report=xml:pytest_reports/coverage.xml --cov-report=html:pytest_reports/coverage/ --junitxml=pytest_reports/junit.xml --html=pytest_reports/rsgisib_tests_report.html --self-contained-html
+
+coverage run -m pytest -v python_tests/test_classification_classsklearn.py
+coverage xml -o pytest_reports/coverage.xml
+
 sonar-scanner \
   -Dsonar.organization=remotesensinginfo \
   -Dsonar.projectKey=remotesensinginfo_rsgislib \
   -Dsonar.sources=python/rsgislib \
-  -Dsonar.host.url=https://sonarcloud.io
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.python.coverage.reportPaths=pytest_reports/coverage.xml
