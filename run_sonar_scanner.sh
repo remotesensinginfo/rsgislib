fe8108f319e59957549fd06cd0ed13dae61d3e44
Pete Bunting <petebunting@mac.com>
2024-01-04 20:50:39 +0000

    Add script to run sonar scanner.

---
+++ run_sonar_scanner.sh
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.organization=remotesensinginfo \
+  -Dsonar.projectKey=remotesensinginfo_rsgislib \
+  -Dsonar.sources=python/rsgislib \
+  -Dsonar.host.url=https://sonarcloud.io
