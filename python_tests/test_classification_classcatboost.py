801e7f1ef0bfea051ecdb145973a992394a83485
Pete Bunting <petebunting@mac.com>
2024-01-05 15:43:33 +0000

    Added tests for roughness and catboost functions.

---
+++ python_tests/test_classification_classcatboost.py
@@ -0,0 +1,323 @@
+import os
+import pytest
+
+H5PY_NOT_AVAIL = False
+try:
+    import h5py
+except ImportError:
+    H5PY_NOT_AVAIL = True
+
+CATBOOST_NOT_AVAIL = False
+try:
+    import catboost
+except ImportError:
+    CATBOOST_NOT_AVAIL = True
+
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+CLASSIFICATION_DATA_DIR = os.path.join(DATA_DIR, "classification")
+
+
+@pytest.mark.skipif(
+    (H5PY_NOT_AVAIL or CATBOOST_NOT_AVAIL),
+    reason="h5py or catboost dependencies not available",
+)
+def test_train_catboost_binary_classifier(tmp_path):
+    import rsgislib.classification.classcatboost
+
+    mdl_cls_obj = rsgislib.classification.classcatboost.get_catboost_mdl()
+
+    cls1_train_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_train.h5"
+    )
+    cls1_valid_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_valid.h5"
+    )
+    cls1_test_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_test.h5"
+    )
+    cls2_train_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_train.h5"
+    )
+    cls2_valid_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_valid.h5"
+    )
+    cls2_test_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_test.h5"
+    )
+
+    out_mdl_file = os.path.join(tmp_path, "out_mdl_file.json")
+    rsgislib.classification.classcatboost.train_catboost_binary_classifier(
+        mdl_cls_obj,
+        cls1_train_file,
+        cls1_valid_file,
+        cls1_test_file,
+        cls2_train_file,
+        cls2_valid_file,
+        cls2_test_file,
+        cat_cols=None,
+        out_mdl_file=out_mdl_file,
+        verbose_training=False,
+    )
+
+    assert os.path.exists(out_mdl_file)
+
+
+@pytest.mark.skipif(
+    (H5PY_NOT_AVAIL or CATBOOST_NOT_AVAIL),
+    reason="h5py or catboost dependencies not available",
+)
+def test_apply_catboost_binary_classifier(tmp_path):
+    import rsgislib.classification.classcatboost
+    import rsgislib.imageutils
+
+    mdl_cls_obj = rsgislib.classification.classcatboost.get_catboost_mdl()
+
+    cls1_train_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_train.h5"
+    )
+    cls1_valid_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_valid.h5"
+    )
+    cls1_test_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_test.h5"
+    )
+    cls2_train_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_train.h5"
+    )
+    cls2_valid_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_valid.h5"
+    )
+    cls2_test_file = os.path.join(
+        CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_test.h5"
+    )
+
+    out_mdl_file = os.path.join(tmp_path, "out_mdl_file.json")
+    rsgislib.classification.classcatboost.train_catboost_binary_classifier(
+        mdl_cls_obj,
+        cls1_train_file,
+        cls1_valid_file,
+        cls1_test_file,
+        cls2_train_file,
+        cls2_valid_file,
+        cls2_test_file,
+        cat_cols=None,
+        out_mdl_file=out_mdl_file,
+        verbose_training=False,
+    )
+
+    in_msk_img = os.path.join(DATA_DIR, "sen2_20210527_aber_subset_vldmsk.kea")
+    s2_img = os.path.join(DATA_DIR, "sen2_20210527_aber_subset.kea")
+
+    img_band_info = []
+    img_band_info.append(
+        rsgislib.imageutils.ImageBandInfo(s2_img, "s2", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    )
+
+    out_prob_img = os.path.join(tmp_path, "out_prob_cls.kea")
+    out_class_img = os.path.join(tmp_path, "out_cls_img.kea")
+
+    rsgislib.classification.classcatboost.apply_catboost_binary_classifier(
+        mdl_cls_obj,
+        in_msk_img,
+        1,
+        img_band_info,
+        out_class_img,
+        "KEA",
+        out_prob_img,
+    )
+
+    assert os.path.exists(out_prob_img) and os.path.exists(out_class_img)
+
+
+@pytest.mark.skipif(
+    (H5PY_NOT_AVAIL or CATBOOST_NOT_AVAIL),
+    reason="h5py or catboost dependencies not available",
+)
+def test_train_catboost_multiclass_classifier(tmp_path):
+    import rsgislib.classification.classcatboost
+
+    cls_info_dict = dict()
+    cls_info_dict["Forest"] = rsgislib.classification.ClassInfoObj(
+        id=0,
+        out_id=1,
+        train_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_train.h5"
+        ),
+        test_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_test.h5"
+        ),
+        valid_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_valid.h5"
+        ),
+        red=120,
+        green=120,
+        blue=120,
+    )
+    cls_info_dict["Grass"] = rsgislib.classification.ClassInfoObj(
+        id=1,
+        out_id=2,
+        train_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_train.h5"
+        ),
+        test_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_test.h5"
+        ),
+        valid_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_valid.h5"
+        ),
+        red=120,
+        green=120,
+        blue=120,
+    )
+    cls_info_dict["Urban"] = rsgislib.classification.ClassInfoObj(
+        id=2,
+        out_id=3,
+        train_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_urban_smpls_bal_train.h5"
+        ),
+        test_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_urban_smpls_bal_test.h5"
+        ),
+        valid_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_urban_smpls_bal_valid.h5"
+        ),
+        red=120,
+        green=120,
+        blue=120,
+    )
+    cls_info_dict["Water"] = rsgislib.classification.ClassInfoObj(
+        id=3,
+        out_id=4,
+        train_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_water_smpls_bal_train.h5"
+        ),
+        test_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_water_smpls_bal_test.h5"
+        ),
+        valid_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_water_smpls_bal_valid.h5"
+        ),
+        red=120,
+        green=120,
+        blue=120,
+    )
+
+    mdl_cls_obj = rsgislib.classification.classcatboost.get_catboost_mdl()
+    out_mdl_file = os.path.join(tmp_path, "out_mdl_file.json")
+    rsgislib.classification.classcatboost.train_catboost_multiclass_classifier(
+        mdl_cls_obj,
+        cls_info_dict,
+        cat_cols=None,
+        out_mdl_file=out_mdl_file,
+        verbose_training=False,
+    )
+
+    assert os.path.exists(out_mdl_file)
+
+
+@pytest.mark.skipif(
+    (H5PY_NOT_AVAIL or CATBOOST_NOT_AVAIL),
+    reason="h5py or catboost dependencies not available",
+)
+def test_apply_catboost_multiclass_classifier(tmp_path):
+    import rsgislib.classification.classcatboost
+    import rsgislib.imageutils
+
+    cls_info_dict = dict()
+    cls_info_dict["Forest"] = rsgislib.classification.ClassInfoObj(
+        id=0,
+        out_id=1,
+        train_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_train.h5"
+        ),
+        test_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_test.h5"
+        ),
+        valid_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_forest_smpls_bal_valid.h5"
+        ),
+        red=120,
+        green=120,
+        blue=120,
+    )
+    cls_info_dict["Grass"] = rsgislib.classification.ClassInfoObj(
+        id=1,
+        out_id=2,
+        train_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_train.h5"
+        ),
+        test_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_test.h5"
+        ),
+        valid_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_grass_smpls_bal_valid.h5"
+        ),
+        red=120,
+        green=120,
+        blue=120,
+    )
+    cls_info_dict["Urban"] = rsgislib.classification.ClassInfoObj(
+        id=2,
+        out_id=3,
+        train_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_urban_smpls_bal_train.h5"
+        ),
+        test_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_urban_smpls_bal_test.h5"
+        ),
+        valid_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_urban_smpls_bal_valid.h5"
+        ),
+        red=120,
+        green=120,
+        blue=120,
+    )
+    cls_info_dict["Water"] = rsgislib.classification.ClassInfoObj(
+        id=3,
+        out_id=4,
+        train_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_water_smpls_bal_train.h5"
+        ),
+        test_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_water_smpls_bal_test.h5"
+        ),
+        valid_file_h5=os.path.join(
+            CLASSIFICATION_DATA_DIR, "cls_water_smpls_bal_valid.h5"
+        ),
+        red=120,
+        green=120,
+        blue=120,
+    )
+
+    mdl_cls_obj = rsgislib.classification.classcatboost.get_catboost_mdl()
+    out_mdl_file = os.path.join(tmp_path, "out_mdl_file.json")
+    rsgislib.classification.classcatboost.train_catboost_multiclass_classifier(
+        mdl_cls_obj,
+        cls_info_dict,
+        cat_cols=None,
+        out_mdl_file=out_mdl_file,
+        verbose_training=False,
+    )
+
+    in_msk_img = os.path.join(DATA_DIR, "sen2_20210527_aber_subset_vldmsk.kea")
+    s2_img = os.path.join(DATA_DIR, "sen2_20210527_aber_subset.kea")
+
+    img_band_info = []
+    img_band_info.append(
+        rsgislib.imageutils.ImageBandInfo(s2_img, "s2", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    )
+
+    out_class_img = os.path.join(tmp_path, "out_cls_img.kea")
+    rsgislib.classification.classcatboost.apply_catboost_multiclass_classifier(
+        mdl_cls_obj,
+        cls_info_dict,
+        in_msk_img,
+        1,
+        img_band_info,
+        out_class_img,
+        gdalformat="KEA",
+        class_clr_names=True,
+    )
+
+    assert os.path.exists(out_class_img)
---
+++ python_tests/test_elevation_roughness.py
@@ -0,0 +1,363 @@
+import os
+import glob
+import pytest
+
+MATPLOTLIB_NOT_AVAIL = False
+try:
+    import matplotlib.pyplot
+except ImportError:
+    MATPLOTLIB_NOT_AVAIL = True
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "elevation")
+ROUGH_DATA_DIR = os.path.join(DATA_DIR, "roughness")
+
+
+def test_calc_simple_roughness_profile_metrics(tmp_path):
+    import rsgislib.elevation.roughness
+    import rsgislib.vectorattrs
+
+    input_dem_img = os.path.join(DATA_DIR, "SRTM_aber.kea")
+    vec_file = os.path.join(ROUGH_DATA_DIR, "rough_smpl_pts.geojson")
+    vec_lyr = "rough_smpl_pts"
+
+    out_vec_file = os.path.join(tmp_path, "out_pts.geojson")
+    out_vec_lyr = "out_pts"
+
+    rsgislib.elevation.roughness.calc_simple_roughness_profile_metrics(
+        input_dem_img,
+        vec_file,
+        vec_lyr,
+        out_vec_file,
+        out_vec_lyr,
+        out_format="GeoJSON",
+        interp_method=rsgislib.INTERP_CUBIC,
+        angle_col=None,
+        x_box_col="xbox",
+        y_box_col="ybox",
+        no_data_val=0,
+        detrend=True,
+        detrend_poly_order=1,
+        export_plot_path=None,
+    )
+
+    x_MIF_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="x_MIF"
+    )
+    y_rv_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="y_rv"
+    )
+    sum_rp_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="sum_rp"
+    )
+    avg_rz_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="avg_rz"
+    )
+    x_rsk_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="x_rsk"
+    )
+    avg_rku_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="avg_rku"
+    )
+
+    assert (
+        (len(x_MIF_arr) == 3)
+        and (len(y_rv_arr) == 3)
+        and (len(sum_rp_arr) == 3)
+        and (len(avg_rz_arr) == 3)
+        and (len(x_rsk_arr) == 3)
+        and (len(avg_rku_arr) == 3)
+    )
+
+@pytest.mark.skipif(MATPLOTLIB_NOT_AVAIL, reason="matplotlib dependency not available")
+def test_calc_simple_roughness_profile_metrics_plots(tmp_path):
+    import rsgislib.elevation.roughness
+    import rsgislib.vectorattrs
+
+    input_dem_img = os.path.join(DATA_DIR, "SRTM_aber.kea")
+    vec_file = os.path.join(ROUGH_DATA_DIR, "rough_smpl_pts.geojson")
+    vec_lyr = "rough_smpl_pts"
+
+    out_vec_file = os.path.join(tmp_path, "out_pts.geojson")
+    out_vec_lyr = "out_pts"
+
+    out_plots_dir = os.path.join(tmp_path, "plots")
+    if not os.path.exists(out_plots_dir):
+        os.mkdir(out_plots_dir)
+
+    rsgislib.elevation.roughness.calc_simple_roughness_profile_metrics(
+        input_dem_img,
+        vec_file,
+        vec_lyr,
+        out_vec_file,
+        out_vec_lyr,
+        out_format="GeoJSON",
+        interp_method=rsgislib.INTERP_CUBIC,
+        angle_col=None,
+        x_box_col="xbox",
+        y_box_col="ybox",
+        no_data_val=0,
+        detrend=True,
+        detrend_poly_order=1,
+        export_plot_path=out_plots_dir,
+    )
+
+    plot_imgs = glob.glob(os.path.join(out_plots_dir, "*.png"))
+
+    assert len(plot_imgs) == 3
+
+
+def test_calc_munro_roughness_profile_metrics(tmp_path):
+    import rsgislib.elevation.roughness
+    import rsgislib.vectorattrs
+
+    input_dem_img = os.path.join(DATA_DIR, "SRTM_aber.kea")
+    vec_file = os.path.join(ROUGH_DATA_DIR, "rough_smpl_pts.geojson")
+    vec_lyr = "rough_smpl_pts"
+
+    out_vec_file = os.path.join(tmp_path, "out_pts.geojson")
+    out_vec_lyr = "out_pts"
+
+    rsgislib.elevation.roughness.calc_munro_roughness_profile_metrics(
+        input_dem_img,
+        vec_file,
+        vec_lyr,
+        out_vec_file,
+        out_vec_lyr,
+        out_format="GeoJSON",
+        interp_method=rsgislib.INTERP_CUBIC,
+        angle_col=None,
+        x_box_col="xbox",
+        y_box_col="ybox",
+        no_data_val=0,
+        detrend=True,
+        detrend_poly_order=1,
+        export_plot_path=None,
+    )
+
+    x_munro_peaks_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="x_munro_peaks"
+    )
+    y_munro_area_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="y_munro_area"
+    )
+    sum_munro_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="sum_munro_z0"
+    )
+    avg_munro_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="avg_munro_z0"
+    )
+
+    assert (
+        (len(x_munro_peaks_arr) == 3)
+        and (len(y_munro_area_arr) == 3)
+        and (len(sum_munro_z0_arr) == 3)
+        and (len(avg_munro_z0_arr) == 3)
+    )
+
+@pytest.mark.skipif(MATPLOTLIB_NOT_AVAIL, reason="matplotlib dependency not available")
+def test_calc_munro_roughness_profile_metrics_plots(tmp_path):
+    import rsgislib.elevation.roughness
+    import rsgislib.vectorattrs
+
+    input_dem_img = os.path.join(DATA_DIR, "SRTM_aber.kea")
+    vec_file = os.path.join(ROUGH_DATA_DIR, "rough_smpl_pts.geojson")
+    vec_lyr = "rough_smpl_pts"
+
+    out_vec_file = os.path.join(tmp_path, "out_pts.geojson")
+    out_vec_lyr = "out_pts"
+
+    out_plots_dir = os.path.join(tmp_path, "plots")
+    if not os.path.exists(out_plots_dir):
+        os.mkdir(out_plots_dir)
+
+    rsgislib.elevation.roughness.calc_munro_roughness_profile_metrics(
+        input_dem_img,
+        vec_file,
+        vec_lyr,
+        out_vec_file,
+        out_vec_lyr,
+        out_format="GeoJSON",
+        interp_method=rsgislib.INTERP_CUBIC,
+        angle_col=None,
+        x_box_col="xbox",
+        y_box_col="ybox",
+        no_data_val=0,
+        detrend=True,
+        detrend_poly_order=1,
+        export_plot_path=out_plots_dir,
+    )
+
+    plot_imgs = glob.glob(os.path.join(out_plots_dir, "*.png"))
+
+    assert len(plot_imgs) == 3
+
+
+def test_calc_smith_roughness_metrics(tmp_path):
+    import rsgislib.elevation.roughness
+    import rsgislib.vectorattrs
+
+    input_dem_img = os.path.join(DATA_DIR, "SRTM_aber.kea")
+    vec_file = os.path.join(ROUGH_DATA_DIR, "rough_smpl_pts.geojson")
+    vec_lyr = "rough_smpl_pts"
+
+    out_vec_file = os.path.join(tmp_path, "out_pts.geojson")
+    out_vec_lyr = "out_pts"
+
+    rsgislib.elevation.roughness.calc_smith_roughness_metrics(
+        input_dem_img,
+        vec_file,
+        vec_lyr,
+        out_vec_file,
+        out_vec_lyr,
+        out_format="GeoJSON",
+        interp_method=rsgislib.INTERP_CUBIC,
+        angle_col=None,
+        x_box_col="xbox",
+        y_box_col="ybox",
+        no_data_val=0,
+        detrend=True,
+        export_plot_path=None,
+    )
+
+    smith_hs_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="smith_hs"
+    )
+    xe_smith_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="xe_smith_z0"
+    )
+    ys_smith_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="ys_smith_z0"
+    )
+    avg_smith_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="avg_smith_z0"
+    )
+
+    assert (
+        (len(smith_hs_arr) == 3)
+        and (len(xe_smith_z0_arr) == 3)
+        and (len(ys_smith_z0_arr) == 3)
+        and (len(avg_smith_z0_arr) == 3)
+    )
+
+def test_calc_smith_roughness_metrics_plots(tmp_path):
+    import rsgislib.elevation.roughness
+    import rsgislib.vectorattrs
+
+    input_dem_img = os.path.join(DATA_DIR, "SRTM_aber.kea")
+    vec_file = os.path.join(ROUGH_DATA_DIR, "rough_smpl_pts.geojson")
+    vec_lyr = "rough_smpl_pts"
+
+    out_vec_file = os.path.join(tmp_path, "out_pts.geojson")
+    out_vec_lyr = "out_pts"
+
+    out_plots_dir = os.path.join(tmp_path, "plots")
+    if not os.path.exists(out_plots_dir):
+        os.mkdir(out_plots_dir)
+
+    rsgislib.elevation.roughness.calc_smith_roughness_metrics(
+        input_dem_img,
+        vec_file,
+        vec_lyr,
+        out_vec_file,
+        out_vec_lyr,
+        out_format="GeoJSON",
+        interp_method=rsgislib.INTERP_CUBIC,
+        angle_col=None,
+        x_box_col="xbox",
+        y_box_col="ybox",
+        no_data_val=0,
+        detrend=True,
+        export_plot_path=out_plots_dir,
+    )
+
+    plot_imgs = glob.glob(os.path.join(out_plots_dir, "*.png"))
+
+    assert len(plot_imgs) == 3
+
+
+def test_calc_all_roughness_profile_metrics(tmp_path):
+    import rsgislib.elevation.roughness
+    import rsgislib.vectorattrs
+
+    input_dem_img = os.path.join(DATA_DIR, "SRTM_aber.kea")
+    vec_file = os.path.join(ROUGH_DATA_DIR, "rough_smpl_pts.geojson")
+    vec_lyr = "rough_smpl_pts"
+
+    out_vec_file = os.path.join(tmp_path, "out_pts.geojson")
+    out_vec_lyr = "out_pts"
+
+    rsgislib.elevation.roughness.calc_all_roughness_profile_metrics(
+        input_dem_img,
+        vec_file,
+        vec_lyr,
+        out_vec_file,
+        out_vec_lyr,
+        out_format="GeoJSON",
+        interp_method=rsgislib.INTERP_CUBIC,
+        angle_col=None,
+        x_box_col="xbox",
+        y_box_col="ybox",
+        no_data_val=0,
+        detrend=True,
+        detrend_poly_order=1,
+    )
+
+    x_MIF_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="x_MIF"
+    )
+    y_rv_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="y_rv"
+    )
+    sum_rp_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="sum_rp"
+    )
+    avg_rz_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="avg_rz"
+    )
+    x_rsk_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="x_rsk"
+    )
+    avg_rku_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="avg_rku"
+    )
+    x_munro_peaks_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="x_munro_peaks"
+    )
+    y_munro_area_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="y_munro_area"
+    )
+    sum_munro_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="sum_munro_z0"
+    )
+    avg_munro_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="avg_munro_z0"
+    )
+    smith_hs_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="smith_hs"
+    )
+    xe_smith_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="xe_smith_z0"
+    )
+    ys_smith_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="ys_smith_z0"
+    )
+    avg_smith_z0_arr = rsgislib.vectorattrs.read_vec_column(
+        out_vec_file, out_vec_lyr, att_column="avg_smith_z0"
+    )
+
+    assert (
+        (len(x_MIF_arr) == 3)
+        and (len(y_rv_arr) == 3)
+        and (len(sum_rp_arr) == 3)
+        and (len(avg_rz_arr) == 3)
+        and (len(x_rsk_arr) == 3)
+        and (len(avg_rku_arr) == 3)
+        and (len(x_munro_peaks_arr) == 3)
+        and (len(y_munro_area_arr) == 3)
+        and (len(sum_munro_z0_arr) == 3)
+        and (len(avg_munro_z0_arr) == 3)
+        and (len(smith_hs_arr) == 3)
+        and (len(xe_smith_z0_arr) == 3)
+        and (len(ys_smith_z0_arr) == 3)
+        and (len(avg_smith_z0_arr) == 3)
+    )
